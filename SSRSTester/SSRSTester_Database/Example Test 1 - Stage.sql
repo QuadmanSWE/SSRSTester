75187170b46a16594a257de41c5d3f795afdf91f
QuadmanSWE <quadmanswe@gmail.com>
2017-10-10 10:37:18 +0200

    Added SQL examples. One for using staged information and one for using local report server information.

---
+++ SSRSTester/SSRSTester_Database/Example Test 1 - Stage.sql
@@ -0,0 +1,15 @@
+﻿--Copies data from staging and sets up a test containing all reports.
+-- Make sure the target deployment state is what you have loaded to stage.
+DECLARE @testId int = 1;
+INSERT INTO [Testing].[Reports]
+(
+ItemID, OldReportPath, OldReportName, OldReportParentID, OldReportDescription, OldReportHidden, OldReportCreatedByID, OldReportCreationDate, OldReportModifiedByID, OldReportModifiedDate, OldReportParameter, OldReportPolicyID, OldReportPolicyRoot
+)
+SELECT 
+ItemID, Path, Name, ParentID, Description, Hidden, CreatedByID, CreationDate, ModifiedByID, ModifiedDate, Parameter, PolicyID, PolicyRoot
+FROM [Stage].[ReportMetaData];
+
+INSERT INTO [Testing].[ReportTests] (ItemID, [TestID], [ExistsInProject])
+SELECT ItemId, @testId AS TestId, 0 AS [ExistsInProject] FROM [Testing].[Reports];
+
+EXECUTE [Testing].[usp_SetDeploymentStatusOnTest] @testId; --if you are not happy with the match between the reports and what you deployed you either have to change your deployment strategy or set up a routine of how to move or rename reports
---
+++ SSRSTester/SSRSTester_Database/Example Test 2 - Local.sql
@@ -0,0 +1,13 @@
+﻿--Copies data from the local ReportServer.
+DECLARE @testId int = 2;
+INSERT INTO [Testing].[Reports]
+(
+ItemID, OldReportPath, OldReportName, OldReportParentID, OldReportDescription, OldReportHidden, OldReportCreatedByID, OldReportCreationDate, OldReportModifiedByID, OldReportModifiedDate, OldReportParameter, OldReportPolicyID, OldReportPolicyRoot
+)
+SELECT 
+ItemID, Path, Name, ParentID, Description, Hidden, CreatedByID, CreationDate, ModifiedByID, ModifiedDate, Parameter, PolicyID, PolicyRoot
+FROM [$(ReportingServicesDummy)].dbo.Catalog WHERE type = 2;
+
+INSERT INTO [Testing].[ReportTests] (ItemID, [TestID], [ExistsInProject])
+SELECT ItemId, @testId AS TestId, 1 AS [ExistsInProject] FROM [Testing].[Reports]; --we already know everything exists, no need to run the procedure as in example 1.
+
